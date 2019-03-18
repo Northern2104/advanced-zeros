fa5d2e6be320fd40751ca020347509f022bece8d
Northern2104 <shinigami210486@gmail.com>
2019-03-18 20:18:41 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+ let count=0;
+	let n = base;
+	do{	
+		if(n==2){break};
+		n=n/2;
+	}while(n%2==0);
+	for(i=1;i<=number;i++){
+		if(i%n==0){
+			let a = i;
+			do{
+				a=a/n;
+				count+=1;
+			} while(a%n==0);
+		}	
+	}
+	return count;
 }
