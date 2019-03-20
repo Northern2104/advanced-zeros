cb3aa4279fa9aa15a5ff27874f1c1aaa7da2be32
Northern2104 <shinigami210486@gmail.com>
2019-03-20 22:43:13 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
 module.exports = function getZerosCount(number, base) {
  let count=0;
-	let n = base;
-	do{	
-		if(n==2){break};
-		n=n/2;
-	}while(n%2==0);
-	
-	if(n%3==0)
-		do{	
-			if(n==3){break};
-			n=n/3;
-		}while(n%3==0);
-	if(n%5==0)
-		do{	
-			if(n==5){break};
-			n=n/5;
-		}while(n%5==0);
-	if(n%7==0)
-		do{	
-			if(n==7){break};
-			n=n/7;
-		}while(n%7==0);
-	
+ let numbers = [67,61,59,53,47,43,41,37,31,29,23,19,17,13,11,7,5,3,2];
+ let n = 0;
+ for(i=0;i<numbers.length;i++){
+ 	if(base%numbers[i]==0){
+ 		n=numbers[i];
+ 		break;
+ 	}
+ }
 
 	for(i=1;i<=number;i++){
 		if(i%n==0){
